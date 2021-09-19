4550e681866351a7033383d33b70dea757fe58f6
Tatsuya Yamaguchi <t-yamaguchi@cloudpack.jp>
2021-09-19 13:14:55 +0900

    xxx-Swift.hが見つからない問題のW/A
    詳細はこのIssueを参照
    https://github.com/flutter/flutter/issues/25676

--- ios/Classes/AppReviewPlugin.m
+++ ios/Classes/AppReviewPlugin.m
@@ -1,5 +1,10 @@
 #import "AppReviewPlugin.h"
+
+#if __has_include(<app_review/app_review-Swift.h>)
 #import <app_review/app_review-Swift.h>
+#else
+#import "app_review-Swift.h"
+#endif
 
 @implementation AppReviewPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
